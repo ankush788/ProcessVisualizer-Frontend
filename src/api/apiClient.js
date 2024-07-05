4d99ddaddb1a5293881acc450c62b98bad323cd6
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-05 19:34:02 +0530

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,10 +1,27 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://process-visualizer-backend-3he2knjt3-ankush788s-projects.vercel.app",
+  baseURL: "https://process-visualizer-backend.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
-})
+  withCredentials: true, // Include credentials with requests
+});
+
+apiClient.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response) {
+      console.error("Response error:", error.response.data);
+      console.error("Status:", error.response.status);
+      console.error("Headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("Request error:", error.request);
+    } else {
+      console.error("Axios error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
