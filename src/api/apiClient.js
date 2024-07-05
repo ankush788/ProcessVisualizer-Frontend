c8e632a2544bcf28210411a7fc1173465f28323a
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-05 19:10:06 +0530

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://process-visualizer-backend.vercel.app",
+  baseURL: "https://process-visualizer-backend-3he2knjt3-ankush788s-projects.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
